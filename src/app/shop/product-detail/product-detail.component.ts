52448f962d10e6157fd60d0b422110ea35a232a6
Dishant Rana <ranadishant111@gmail.com>
2023-10-18 15:15:58 +0530

    End section 12
    Ui improvement.

--- src/app/shop/product-detail/product-detail.component.ts
+++ src/app/shop/product-detail/product-detail.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ShopService } from '../shop.service';
 import { Product } from './../../shared/Models/product';
 import { Component, OnInit } from '@angular/core';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-detail',
@@ -14,17 +15,26 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(
     private shopservice:ShopService, 
-    private actiavatedRoute:ActivatedRoute){
+    private actiavatedRoute:ActivatedRoute,
+    private bcService: BreadcrumbService){
 
+      this.bcService.set('@productDetails', " ");
   }
   
   ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
     const id = this.actiavatedRoute.snapshot.paramMap.get("id");
-    
-    if (id) 
+
+    if (id)
       this.shopservice.getProduct(+id).subscribe({
-        next: product => this.product = product,
-        error : error=>console.log(error)
+        next: product => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        error: error => console.log(error)
       });
   }
 
